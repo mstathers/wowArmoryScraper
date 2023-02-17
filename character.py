3dbb2ed2ac86a7c6dde8264beee258a6a4c3ab74
Michael Stathers <mstathers@outlook.com>
2023-02-16 23:56:35 -0800

    New character class new 10.0.5 roster for mythic

---
+++ character.py
@@ -0,0 +1,84 @@
+import requests
+import urllib.parse
+import json
+
+
+class character:
+    name: str = ""
+    realm: str = ""
+    WOWHEAD = "https://worldofwarcraft.com/en-us/character/us/"
+    def __init__(self, name: str, realm: str) -> None:
+        self.name = name
+        self.realm = realm
+
+    @property
+    def armoryUrl(self) -> str:
+        quotedName = urllib.parse.quote(self.name)
+        return f"{self.WOWHEAD}{self.realm}/{quotedName}"
+
+    @property
+    def characterJson(self) -> dict:
+        try:
+            r = requests.get(self.armoryUrl)
+            r.raise_for_status()
+        except:
+            return {self.name: "?"}
+
+        charJson = {}
+        for line in r.text.split("\n"):
+            if 'characterProfileInitialState = ' in line:
+                charJson = json.loads(line.partition('characterProfileInitialState = ')[2].rstrip(";"))
+                break
+
+        if not charJson:
+            raise Exception("Characater information not found.")
+
+        return charJson
+
+    @property
+    def itemLevel(self) -> str:
+        if 'character' not in self.characterJson:
+            return "Average Item Level Unknown."
+
+        return self.characterJson['character']['averageItemLevel']
+
+
+    def tierBonuses(self) -> dict:
+        if 'character' not in self.characterJson:
+            return {self.name: "?"}
+
+
+        TIER_SLOTS = [
+            "chest",
+            "head",
+            "shoulder",
+            "hand",
+            "leg"
+        ]
+
+
+        setCount = 0
+        for slot in TIER_SLOTS:
+            if setCount > 0:
+                break
+            if "set" in self.characterJson['character']['gear'][slot]:
+                for index in range(len(self.characterJson['character']['gear'][slot]['set']['effects'])):
+                    if "is_active" in self.characterJson['character']['gear'][slot]['set']['effects'][index]:
+                        setCount = self.characterJson['character']['gear'][slot]['set']['effects'][index]['required_count']
+                #out = self.characterJson['character']['gear'][slot]['set']['effects'][0]
+                #print(json.dumps(out, indent=4))
+
+        #print("{}: {}".format(self.name, setCount))
+        return {self.name: {"Tier Set Bonus": setCount, "Average Item Level": self.itemLevel}}
+
+
+
+
+#pitza = character(name="pitza", realm="elune")
+
+#print(pitza.name)
+#print(pitza.realm)
+#print(pitza.armoryUrl)
+#print(json.dumps(pitza.characterJson, sort_keys=True, indent=4))
+#print(pitza.tierBonuses())
+
---
+++ tierScraper-10.0.5-mythic.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+
+import multiprocessing
+from character import character
+
+
+def main() -> None:
+    roster = [
+        {"name":"Pitza", "realm":"elune"},
+        {"name":"Phorcefull", "realm":"elune"},
+        {"name":"Aknologia", "realm":"elune"},
+        {"name":"Callippe", "realm":"elune"},
+        {"name":"Fog", "realm":"elune"},
+        {"name":"Halîk", "realm":"nathrezim"},
+        {"name":"Citra", "realm":"gilneas"},
+        {"name":"Mayday", "realm":"gilneas"},
+        {"name":"Murfwyrm", "realm":"elune"},
+        {"name":"Murfbot", "realm":"elune"},
+        {"name":"Duero", "realm":"sargeras"},
+        {"name":"Dochollidày", "realm":"elune"},
+        {"name":"Aya", "realm":"elune"},
+        {"name":"Rhenin", "realm":"elune"},
+        {"name":"Dehvii", "realm":"elune"},
+        {"name":"Strozzoun", "realm":"elune"},
+        {"name":"Astartes", "realm":"elune"},
+        {"name":"Comelywhite", "realm":"elune"},
+        {"name":"Zordiak", "realm":"elune"},
+        {"name":"Mockra", "realm":"elune"},
+        {"name":"Danabell", "realm":"elune"},
+        {"name":"Baryll", "realm":"elune"},
+        {"name":"Beansupplier", "realm":"illidan"},
+        {"name":"Chamanita", "realm":"elune"},
+        {"name":"Haralda", "realm":"elune"},
+        {"name":"Fira", "realm":"elune"},
+    ]
+
+    pool_size = multiprocessing.cpu_count()
+    pool = multiprocessing.Pool(pool_size)
+    rosterTier = pool.map(worker, roster)
+    pool.close()
+    pool.join()
+
+
+    # Output
+    sortedTier = sorted(rosterTier, key=lambda d: list(d.keys()))
+    for i in sortedTier:
+        for key, value in i.items():
+            print(f"{key} {value}")
+
+
+def worker(player: dict) -> dict:
+    toon = character(name=player['name'], realm=player['realm'])
+    return toon.tierBonuses()
+
+
+if __name__ == "__main__":
+    main()
